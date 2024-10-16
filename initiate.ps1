b9399453d41ad0df097c66858298b48c81aba2c9
Yojana <yojana.hawaii@gmail.com>
2024-10-15 14:47:36 -1000

    sync initial file

--- initiate.ps1
+++ initiate.ps1
@@ -1,28 +1,8 @@
-# import only specific functions in the future -> only users if I am working on users
 
-$public             = @(Get-ChildItem -Path "$PWD\public\*.ps1"     -ErrorAction SilentlyContinue -Recurse)
-$private            = @(Get-ChildItem -Path "$PWD\private\*.ps1"    -ErrorAction SilentlyContinue -Recurse)
-$shared             = @(Get-ChildItem -Path "$PWD\shared\*.ps1"     -ErrorAction SilentlyContinue -Recurse)
-$config             = @(Get-ChildItem -Path "$PWD\config-helper\*.ps1"     -ErrorAction SilentlyContinue -Recurse)
-$StoredProcedure    = @(Get-ChildItem -Path "$PWD\stored-procedure\*.ps1"     -ErrorAction SilentlyContinue -Recurse)
-
-
-
-Write-Verbose "Read public, private & shared functions, stored procedures and config helpers"
-#import all function
-foreach ($import in @($public + $private + $shared + $StoredProcedure + $config)){
-    try{
-        . $import.Fullname
-        Write-Verbose "importing $($import.Fullname)" #-Verbose
-    } catch {
-        Write-Error -Message "Failed to import functions from $($import.Fullname): $_"
-        $true
-    }
-    
-}
+# import functions
 
 # $VerbosePreference = "continue"
-$VerbosePreference = "silentlycontinue"
+# $VerbosePreference = "silentlycontinue"
 
 <# using config file #>
 # $conf = Get-fnConfig
@@ -31,16 +11,11 @@ $VerbosePreference = "silentlycontinue"
 # $email.helpdesk
 
 
-<# active-directory #>
-# Add-fnActiveDirectory -Verbose
-
-<# organizational-unit and acl#>
-# Add-fnOrganizationalUnit -verbose
 
 <# Export employee roster #>
 # Export-fnEmployeeToDialMy -Verbose
 # Export-fnLatestEmployeeRosterToRave -Verbose
 
 <# group-policy #>
-Get-fnGPOMetaData #| Format-Table -AutoSize *
+# Get-fnGPOMetaData #| Format-Table -AutoSize *
 # Get-fnGpoContent
