5e9808e53332db6fbeed488c4cc5379bcc573675
Yojana <yojana.hawaii@gmail.com>
2024-11-18 12:53:34 -1000

    future lab bucket, ignore for 90 days.

--- private/files-and-folders/Split-fnOrdersIntoMultipleCsv.ps1
+++ private/files-and-folders/Split-fnOrdersIntoMultipleCsv.ps1
@@ -10,12 +10,21 @@ function Split-fnCsvIntoMultipleCsv {
         [parameter()]
         [string]$group1,
         [parameter()]
-        [string]$group2
+        [string]$group2,
+        [parameter()]
+        [string]$futureLabBucket,
+        [parameter()]
+        [string]$deletedBucket
     )
     Remove-Item -Path $destinationPath -Force
 
-    $today       = Get-Date
-    $dataToGroup = Read-fnCsvDefaultHeader -filename $CsvfileToSplit | Where-Object {(New-TimeSpan -Start $_.performDate -End $today  ).Days -gt $excludeDays }
-    $groupedData = Group-fnIntoTwoPropertyByCount -data $dataToGroup -CountSplit 50 -firstGroup $group1 -secondGroup $group2 
+    $today      = Get-Date
+    $pastDue    = Read-fnCsvDefaultHeader -filename $CsvfileToSplit | Where-Object {(New-TimeSpan -Start $_.performDate -End $today  ).Days -gt $excludeDays }
+    
+    $regular    = $pastDue | Where-Object {$_.bucket -ne $futureLabBucket }
+    $future     = $pastDue | Where-Object {$_.bucket -eq $futureLabBucket -and (New-TimeSpan -Start $_.performDate -End $today  ).Days -gt 90}
+    
+    $dataToGroup = $regular + $future
+    $groupedData = Group-fnIntoTwoPropertyByCount -data $dataToGroup -CountSplit 50 -firstGroup $group1 -secondGroup $group2 -deletedBucket $deletedBucket
     return $groupedData
 }
