243391c99e42818420b7e1144cf696ccdefad08b
Yojana <yojana.hawaii@gmail.com>
2024-11-29 09:46:23 -1000

    message more generic

--- private/files-and-folders/Read-fnCsvDefaultHeader.ps1
+++ private/files-and-folders/Read-fnCsvDefaultHeader.ps1
@@ -5,13 +5,13 @@ function Read-fnCsvDefaultHeader {
         [string]$filename    
     )
 
-    Write-Verbose "Adding additional phone numbers missing in original csv file"
+    Write-Verbose "Reading csv file in Read-fnDefaultHeader.ps1"
 
     if (Test-Path -Path $filename){
         Write-Verbose "Test File path: File exists"
         try {
             $csvData = import-csv -Path $filename -Delimiter ","
-            Write-Verbose "Successfully imported additional phone csv file"
+            Write-Output "Successfully imported csv file."
         } catch {
             Write-Warning "Import CSV file failed: $($_.Exception.Message)"
         }
