05be9ceb7969b089a33e5f00144b98e19198e70a
Yojana <yojana.hawaii@gmail.com>
2024-12-26 15:11:10 -1000

    fix bugs

--- private/clinical/Get-fnInternalOrders.ps1
+++ private/clinical/Get-fnInternalOrders.ps1
@@ -5,11 +5,12 @@ function Get-fnInternalOrders {
         [System.Object]$order
     )
     Write-Information "$($MyInvocation.MyCommand.Name): Separate internal orders using internal order csv file in"
+
     if( Test-Path -Path $order.internalList){
-        $order.internalList = (Read-fnCsvDefaultHeader -filename $order.internalList).order
+        $internalOrders = (Read-fnCsvDefaultHeader -filename $order.internalList).order
         
-        $order.internaldata = $order.externaldata | Where-Object { $_.order -in $order.internalList}
-        $order.externaldata = $order.externaldata | Where-Object { $_.order -notin $order.internalList}
+        $order.internaldata = $order.externaldata | Where-Object { $_.order -in $internalOrders}
+        $order.externaldata = $order.externaldata | Where-Object { $_.order -notin $internalOrders}
 
     } else {
         Write-Warning "$($MyInvocation.MyCommand.Name): Internal order list for $($order.type) does not exist."
--- private/clinical/Set-fnEmailBodyOrderInvalidDestination.ps1
+++ private/clinical/Set-fnEmailBodyOrderInvalidDestination.ps1
@@ -6,17 +6,13 @@ function Set-fnEmailBodyOrderInvalidDestination {
         [parameter()]
         [System.Object]$order
     )
-
     
-    $email.emailBody4 = @{}
     $email.emailBody4["Internal-$($order.type)"] = $order.intDestination
     $email.emailBody4["External-$($order.type)"] = $order.extDestination
     $email.emailBody4["DELETE"] = $order.delDestination
     
-    $email.emailBody5 = $null
     $email.emailBody5 = Join-fnTwoOrderArray -array1 $email.emailBody5 -array2 $order.extSummary -type $order.type -array2_prefix "External"            
     $email.emailBody5 = Join-fnTwoOrderArray -array1 $email.emailBody5 -array2 $order.intSummary -type $order.type -array2_prefix "Internal"      
 
-    $email.emailBody6 = @{}
     $email.emailBody6["$($order.type)"] = $($order.internalList)
 }
